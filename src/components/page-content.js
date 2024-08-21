e6fe5633c1f395227beaee14cc09dde2ea313b1e
ToureBH <tbhcontratos@gmail.com>
2024-08-21 16:38:54 +0200

    First commit

---
+++ src/components/page-content.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Discussion from './discussion';
+import Workflow from './workflow';
+import Rules from './rules';
+
+export default function() {
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={Discussion} />
+                <Route exact path="/rules" component={Rules} />
+                <Route exact path="/workflow" component={Workflow} />
+            </Switch>
+        </div>
+    )
+}
---
+++ src/components/rules.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+     
+     
+export default class Rules extends Component {
+    render() {
+        return <div>Rules...</div>;
+    }
+}
